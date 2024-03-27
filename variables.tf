dde3910816b84ea629d12764a3d52348f1f17707
harsha <nakirikantikiran@outlook.com>
2024-03-27 10:18:24 +0530

    added tf files

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "s3_bucket_name" {
   description = "This is for s3 bucket name"
   type        = list(string)
-  default     = ["my-s3-bucket-52478", "sample-52487", "new-5214785"]
+  default     = ["my-s3-bucket-52478", "sample-52487", "new-5214785", "test-oku-8574"]
 
 }
 
