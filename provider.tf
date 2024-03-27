6c353bdabcd944c6e79b321c0cf7e54ec53bc8e6
harsha <nakirikantikiran@outlook.com>
2024-03-27 10:11:58 +0530

    added tf files

--- provider.tf
+++ provider.tf
@@ -3,6 +3,8 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "5.40.0"
+      AWS_ACCESS_KEY_ID = var.AWS_ACCESS_KEY_ID
+      AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
     }
   }
 }
@@ -11,4 +13,5 @@ provider "aws" {
   # Configuration options
   #region = "us-east1"
   region = "us-east-1"
+  
 }
--- variables.tf
+++ variables.tf
@@ -4,3 +4,11 @@ variable "s3_bucket_name" {
   default     = ["my-s3-bucket-52478", "sample-52487", "new-5214785"]
 
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+
+}
