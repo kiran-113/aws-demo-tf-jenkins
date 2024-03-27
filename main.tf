1b06d2c8bbb1415d73fb93c09cd3f00b1c354f75
harsha <nakirikantikiran@outlook.com>
2024-03-27 09:38:53 +0530

    first push

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "random_string" "random_number" {
+  length  = 4 # Specify the desired length of the hexadecimal string
+  special = false
+}
+
+resource "aws_s3_bucket" "example" {
+  count = length(var.s3_bucket_name)
+  #bucket        = format("${var.s3_bucket_name}-%02d", count.index + 1)
+  bucket        = var.s3_bucket_name[count.index]
+  force_destroy = true
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "s3_bucket_name" {
+  description = "This is for s3 bucket name"
+  type        = list(string)
+  default     = ["my-s3-bucket-52478", "sample-52487", "new-5214785"]
+
+}
